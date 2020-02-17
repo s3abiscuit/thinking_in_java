91c8b8b2fb28b621bca4c42e3ff9c04f200dacc8
s3abiscuit <syjefb2@outlook.com>
2020-02-17 22:16:48 +0800

    add prj Dinner

---
+++ examples/access/Dinner/src/Dinner.java
@@ -0,0 +1,13 @@
+//: access/Dinner.java
+// Uses the library.
+
+import dessert.Cookie;
+
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        //! x.bite(); // Can't access
+    }
+} /* Output:
+Cookie constructor
+*///:~
