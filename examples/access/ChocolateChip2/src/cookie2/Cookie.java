62edde0020229632f43f27a7c5adb469ebca3d1b
s3abiscuit <syjefb2@outlook.com>
2020-02-17 22:24:53 +0800

    make prj ChocolateChip2

---
+++ examples/access/ChocolateChip2/src/cookie2/Cookie.java
@@ -0,0 +1,11 @@
+package cookie2;
+
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+
+    protected void bite() {
+        System.out.println("bite");
+    }
+} ///:~
