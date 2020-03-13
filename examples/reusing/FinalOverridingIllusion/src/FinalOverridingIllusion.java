50e7bbde22f0cc1f593981ecc29387e92e3b92e2
s3abiscuit <syjefb2@outlook.com>
2020-03-13 17:01:34 +0800

    add project FinalOverridingIllusion

--- examples/reusing/FinalOverridingIllusion/src/FinalOverridingIllusion.java
+++ examples/reusing/FinalOverridingIllusion/src/FinalOverridingIllusion.java
@@ -1,7 +1,7 @@
 //: reusing/FinalOverridingIllusion.java
 // It only looks like you can override
 // a private or private final method.
-import static net.mindview.util.Print.*;
+import static util.Print.*;
 
 class WithFinals {
   // Identical to "private" alone:
