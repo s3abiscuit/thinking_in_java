0e5a54d903556d4522e7317fc4cba3d3bc8437ac
s3abiscuit <syjefb2@outlook.com>
2020-03-14 22:44:37 +0800

    Update PrivateOverride.java

--- examples/polymorphism/PrivateOverride/src/PrivateOverride.java
+++ examples/polymorphism/PrivateOverride/src/PrivateOverride.java
@@ -3,8 +3,8 @@
 import static util.Print.*;
 
 public class PrivateOverride {
-  // private method is also final method
-  // can not be overridden
+  // 向上转型的本质
+  // 相对于继承
   private void f() { print("private f()"); }
   public static void main(String[] args) {
     PrivateOverride po = new Derived();
