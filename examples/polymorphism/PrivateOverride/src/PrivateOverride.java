943a586b0e72ebcbeeb5dede3797fed581b974a5
s3abiscuit <syjefb2@outlook.com>
2020-03-15 21:37:32 +0800

    add comments

--- examples/polymorphism/PrivateOverride/src/PrivateOverride.java
+++ examples/polymorphism/PrivateOverride/src/PrivateOverride.java
@@ -3,12 +3,15 @@
 import static util.Print.*;
 
 public class PrivateOverride {
-  // 向上转型的本质
-  // 相对于继承
+  // 向上转型得到的父类对象依然能够调用没有没被覆盖的方法
   private void f() { print("private f()"); }
+  public void g() { print("public g()")}
   public static void main(String[] args) {
+    // 向上转型得到了一个特殊的父类对象
+    // 这个特殊对象在创建子类对象的时候就产生了
     PrivateOverride po = new Derived();
     po.f();
+    po.g();
   }
 }
 
@@ -16,4 +19,5 @@ class Derived extends PrivateOverride {
   public void f() { print("public f()"); }
 } /* Output:
 private f()
+public g()
 *///:~
