e7a2d78d60ff3a0fbee2fe684aa06520a4c633de
s3abiscuit <syjefb2@outlook.com>
2020-03-24 18:50:40 +0800

    chapter16 arrays

--- 16arrays/01ContainerComparison/src/ContainerComparison.java
+++ 16arrays/01ContainerComparison/src/ContainerComparison.java
@@ -1,6 +1,6 @@
 //: arrays/ContainerComparison.java
 import java.util.*;
-import static net.mindview.util.Print.*;
+import static util.Print.*;
 
 class BerylliumSphere {
   private static long counter;
